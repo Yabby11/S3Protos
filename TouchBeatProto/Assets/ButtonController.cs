96e60af7e66a54c2302f3805512578a9bb2f5e09
Jake Andrews <jakeandrews.karatekid@gmail.com>
2016-09-26 23:15:43 +1000

    Rhythem game done

---
+++ TouchBeatProto/Assets/ButtonController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+
+    public GameObject Button1;
+    public GameObject Button2;
+
+    public bool button1Active;
+    public bool button2Active;
+    public bool done;
+
+	// Use this for initialization
+	void Start () {
+
+
+        button1Active = true;
+        button2Active = false;
+        Button2.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+    }
+
+    public void buttonSwap()
+    {
+
+        done = false;
+
+        if(button1Active == true && done == false)
+        {
+
+            button2Active = true;
+            button1Active = false;
+            Debug.Log("Swap1");
+            done = true;
+
+            Button1.SetActive(false);
+            Button2.SetActive(true);
+        }
+        
+        if(button2Active == true && done == false)
+        {
+
+            button1Active = true;
+            button2Active = false;
+            Debug.Log("Swap2");
+            done = true;
+
+            Button1.SetActive(true);
+            Button2.SetActive(false);
+        }
+
+    }
+}
---
+++ TouchBeatProto/Assets/ScrollDown.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollDown : MonoBehaviour {
+
+    public GameObject course;
+    public float speed = 5;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        course.transform.position += Vector3.down * Time.fixedDeltaTime * speed;
+
+    }
+}
